4353e312650b661190de1134c6bf509fc1232ed5
jak010 <bluetoon@naver.com>
2024-02-21 01:41:01 +0900

    update

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 
+from src.controller import controller_router
+
 
 class BoilerPlateApplication:
     app = FastAPI()
 
     def __call__(self, *args, **kwargs):
+        self.app.include_router(controller_router)
+
         return self.app
 
 
-application = BoilerPlateApplication
+application = BoilerPlateApplication()
